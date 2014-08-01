68520a85043c550fa60a85bcd9e705f20d71fd30
sunjian0924 <sunj3@miamioh.edu>
2014-08-01 13:08:49 -0400

    changed the way users interact with the library and moved shareable methods into its prototype

--- examples/student_activity_logging_plugin.js
+++ examples/student_activity_logging_plugin.js
@@ -1,25 +1,19 @@
 var hpit = require('../index');
-hpit = new hpit();
 
-function student_activity_logging_plugin() {
-	this.post_connect = function(next) { 
-		var self = this;
-		self.subscribe({
-			activity_logging: self.log_student_activity_callback
-		}, next);
-	};
-
-	this.log_student_activity_callback = function(message, next) {
-		console.log(message);
-		next();
-	};
-
+function student_activity_logging_plugin(entity_id, api_key, wildcard_callback) {
+	hpit.plugin.call(this, entity_id, api_key, wildcard_callback);
 }
 
 
-student_activity_logging_plugin.prototype = hpit.create_plugin(
-		entity_id = "6bf92c7e-353e-47c2-9690-e3a3c8f24602",
-		api_key = "8abb98929495403a73b3c5a00b4b1a92"
-	);
-
+student_activity_logging_plugin.prototype = Object.create(hpit.plugin.prototype);
+student_activity_logging_plugin.prototype.post_connect = function(next) { 
+	var self = this;
+	self.subscribe({
+		activity_logging: self.log_student_activity_callback
+	}, next);
+};
+student_activity_logging_plugin.prototype.log_student_activity_callback = function(message, next) {
+	console.log(message);
+	next();
+};
 module.exports = student_activity_logging_plugin;
--- examples/student_activity_logging_tutor.js
+++ examples/student_activity_logging_tutor.js
@@ -1,20 +1,16 @@
 var hpit = require('../index');
-hpit = new hpit();
 
-function student_activity_logging_tutor() {
-	this.work = function() {
-		var activity = {subject: "I", verb: "made", object: "it"};
-		this.send(
-				message_name = "activity_logging",
-				payload = activity
-			);
-	};
-
+function student_activity_logging_tutor(entity_id, api_key, callback, attrs) {
+	hpit.tutor.call(this, entity_id, api_key, callback, attrs);
 }
 
 
-student_activity_logging_tutor.prototype = hpit.create_tutor(
-		entity_id = "047f25f1-99fd-4425-9508-9cb21a0955e2",
-		api_key = "0cf68ca2df0e770280d3aacef378ab02"
-	);
+student_activity_logging_tutor.prototype = Object.create(hpit.tutor.prototype);
+student_activity_logging_tutor.prototype.work = function() {
+	var activity = {subject: "I", verb: "made", object: "it"};
+	this.send(
+			message_name = "activity_logging",
+			payload = activity
+		);
+};
 module.exports = student_activity_logging_tutor;

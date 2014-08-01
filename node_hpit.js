302cbf9e1d0d1b461984907fba4c85330b9ded92
sunjian0924 <sunj3@miamioh.edu>
2014-08-01 13:05:58 -0400

    changed the interfaces of the library

--- node_hpit.js
+++ node_hpit.js
@@ -1,32 +1,8 @@
-var tutor = require('./lib/tutor');
-var plugin = require('./lib/plugin');
-function hpit() {
-	//in case users forget to use new keyword
-	if (!(this instanceof hpit)) {
-		return new hpit();
-	}
-}
+(function(exports) {
+	exports.plugin = require('./lib/plugin');
+	exports.tutor = require('./lib/tutor');
 
-hpit.prototype.create_plugin = function(entity_id, api_key, wildcard_callback) {
-	return new plugin(entity_id, api_key, wildcard_callback);
-};
-hpit.prototype.create_tutor = function(entity_id, api_key, callback, attrs) {
-	return new tutor(entity_id, api_key, callback, attrs);
-};
-hpit.prototype.start = function(entity) {
-	entity.start();
-};
-hpit.prototype.stop = function(entity) {
-	entity.stop();
-};
-hpit.prototype.status = function(entity) {
-	if (entity.isRunning()) {
-		return console.log('This entity is running.');
-	}
-	console.log('This entity is not running.');
-};
-module.exports = hpit;
-
+})(typeof exports === 'undefined' ? this._ = this._ || {}: exports);
 
 
 
